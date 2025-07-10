a54c5344bb2b7f800575158028df3cdab56ea78d
Rudy-crw <rudy.wu@outlook.com>
2025-07-10 16:27:04 +0800

    初次上傳 Vite React Todo 專案

---
+++ src/components/TodoWrapper.jsx
@@ -0,0 +1,80 @@
+import { useState } from "react";
+import CreateForm from "./CeateForm";
+import Todo from "./Todo";
+
+function TodoWapper() {
+  const [todos, setTodos] = useState([
+    {
+      content: "打掃廁所",
+      id: Math.random(),
+      isCompleted: true,
+      isEditing: false,
+    },
+    {
+      content: "寫作業",
+      id: Math.random(),
+      isCompleted: false,
+      isEditing: false,
+    },
+  ]);
+  const addTodo = (content) => {
+    setTodos([
+      ...todos,
+      { content: content, id: Math.random(), isEditing: false },
+    ]);
+  };
+  const deleteTodo = (id) => {
+    setTodos(
+      todos.filter((todo) => {
+        return todo.id !== id;
+      })
+    );
+  };
+  const toggleCompleted = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, isCompleted: !todo.isCompleted }
+          : todo;
+      })
+    );
+  };
+
+  const toggleIsEditing = (id) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id ? { ...todo, isEditing: !todo.isEditing } : todo;
+      })
+    );
+  };
+
+  const editTodo = (id, newContent) => {
+    setTodos(
+      todos.map((todo) => {
+        return todo.id === id
+          ? { ...todo, content: newContent, isEditing: false }
+          : todo;
+      })
+    );
+  };
+
+  return (
+    <div className="wrapper">
+      <h1>代辦事項</h1>
+      <CreateForm addTodo={addTodo} />
+      {todos.map((todo) => {
+        return (
+          <Todo
+            toggleCompleted={toggleCompleted}
+            toggleIsEditing={toggleIsEditing}
+            editTodo={editTodo}
+            todo={todo}
+            key={todo.id}
+            deleteTodo={deleteTodo}
+          />
+        );
+      })}
+    </div>
+  );
+}
+export default TodoWapper;
